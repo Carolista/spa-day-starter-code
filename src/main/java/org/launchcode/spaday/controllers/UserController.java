2485cdff7a5a09df64170d39a0e4c03bb4d6074f
Carolista <55961845+Carolista@users.noreply.github.com>
2020-01-09 18:52:21 -0600

    Chapter 13 studio done-ish

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,29 +3,39 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/add")
-    public String displayAddUserForm() {
+    // This was not in the starter code but is necessary for user/index to load
+    // Empty user object added to try to load username when redirecting from add form submission
+//    @GetMapping
+//    public String displayUserPage(Model model) {
+//        model.addAttribute("user", new User());
+//        return "user/index";
+//    }
+
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("user",new User());
         return "user/add";
     }
 
-    @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-           return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
+    @PostMapping("add")
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, Model model, String verify) {
+        if (! verify.equals(user.getPassword())) {
+            model.addAttribute("matchError", "Passwords do not match");
             return "user/add";
+        } else if (errors.hasErrors()) {
+            return "user/add";
+        } else {
+            model.addAttribute("user",user);
+            return "user/index";
         }
 
     }
